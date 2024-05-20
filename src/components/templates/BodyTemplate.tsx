dfc932c6878ac181efa17cbf07232075da77f064
Kuthi <anhthidola@gmail.com>
2024-05-20 23:36:35 +0700

    owari1

---
+++ src/components/templates/BodyTemplate.tsx
@@ -0,0 +1,154 @@
+import React from "react";
+import "../../style/bodyTemplate.css";
+const BodyTemplate = () => {
+  return (
+    <div>
+      <div className="body-container">
+        <div className="flex">
+          <a href="https://shopee.vn/H%E1%BB%99p-%C4%91%E1%BB%B1ng-s%E1%BB%AFa-b%E1%BB%99t-ti%E1%BB%87n-d%E1%BB%A5ng-cho-b%C3%A9-khi-di-ch%C6%A1i-ho%E1%BA%B7c-khi-%C4%91i-ra-ngo%C3%A0i-i.126105032.25180593711?xptdk=5e01b134-ba73-47ac-806b-08ec12d044ed">
+            <div className="body-img">
+              <img src="/images/166.png" alt="" />
+              <div className="ml-[12px] text-[10px]">
+                <p className="font-bold">
+                  Bình trữ sữa cho bé khi đi chơi - Đại
+                </p>
+                <span className="text-red-500 font-medium mr-10">89.000</span>
+                <span>
+                  <del>109.000</del>
+                </span>
+              </div>
+            </div>
+          </a>
+          <a href="https://shopee.vn/H%E1%BB%99p-%C4%91%E1%BB%B1ng-s%E1%BB%AFa-b%E1%BB%99t-ti%E1%BB%87n-d%E1%BB%A5ng-cho-b%C3%A9-khi-di-ch%C6%A1i-ho%E1%BA%B7c-khi-%C4%91i-ra-ngo%C3%A0i-i.126105032.25180593711?xptdk=5e01b134-ba73-47ac-806b-08ec12d044ed">
+            <div>
+              <img src="/images/169.png" alt="" />
+              <div className="ml-[12px] text-[10px]">
+                <p className="font-bold">
+                  Bình trữ sữa cho bé khi đi chơi - Trung
+                </p>
+                <span className="text-red-500 font-medium mr-10">84.000</span>
+                <span>
+                  <del>109.000</del>
+                </span>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="flex">
+          <a href="https://shopee.vn/C%E1%BB%8D-r%E1%BB%ADa-b%C3%ACnh-s%E1%BB%AFa-du-l%E1%BB%8Bch-7-chi-ti%E1%BA%BFt-c%C3%B3-th%E1%BB%83-g%E1%BA%A5p-g%E1%BB%8Dn-k%C3%A8m-h%E1%BB%99p-%C4%91%E1%BB%B1ng-h%C3%ACnh-g%E1%BA%A5u-d%E1%BB%85-th%C6%B0%C6%A1ng-v%C3%A0-gi%C3%A1-%C3%BAp-b%C3%ACnh-ti%E1%BB%87n-l%E1%BB%A3i-i.126105032.25922644159?sp_atk=ea0646a1-0b7b-4724-9f1d-ba0cac109cea&xptdk=ea0646a1-0b7b-4724-9f1d-ba0cac109cea">
+            <div className="body-img">
+              <img src="/images/158.png" alt="" />
+              <div className="ml-[12px] text-[10px]">
+                <p className="font-bold">Cọ rửa bình sữa du lịch</p>
+                <span className="text-red-500 font-medium mr-10">77.000</span>
+                <span>
+                  <del>127.000</del>
+                </span>
+              </div>
+            </div>
+          </a>
+          <a href="https://shopee.vn/C%E1%BB%8D-b%C3%ACnh-s%E1%BB%AFa-d%E1%BA%A1ng-s%E1%BB%91p-l%C3%A0m-s%E1%BA%A1ch-b%C3%ACnh-s%E1%BB%AFa-m%E1%BB%9Bi-cho-tr%E1%BA%BB-s%C6%A1-sinh-gi%C3%A1-%C4%91%E1%BB%B1ng-%C4%91%E1%BB%93-%C4%91%E1%BA%B7c-bi%E1%BB%87t-b%C3%A0n-ch%E1%BA%A3i-tay-c%E1%BA%A7m-d%C3%A0i-b%C3%A0n-ch%E1%BA%A3i-x%E1%BB%91p-i.126105032.22064572850?sp_atk=b7f80a93-e4d8-4603-b3c2-7c55e14dc964&xptdk=b7f80a93-e4d8-4603-b3c2-7c55e14dc964">
+            <div>
+              <img src="/images/150.png" alt="" />
+              <div className="ml-[12px] text-[10px]">
+                <p className="font-bold">Cọ bình sữa dạng sốp</p>
+                <span className="text-red-500 font-medium mr-10">89.000</span>
+                <span>
+                  <del>102.000</del>
+                </span>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="flex">
+          <a href="https://shopee.vn/H%E1%BB%99p-%C4%91%E1%BB%B1ng-s%E1%BB%AFa-b%E1%BB%99t-k%C3%A8m-th%C3%ACa-g%E1%BA%A1t-ti%E1%BB%87n-d%E1%BB%A5ng-h%E1%BB%99p-%C4%91%E1%BB%B1ng-s%E1%BB%AFa-cho-b%C3%A9-khi-%C4%91i-ch%C6%A1i-%C4%91i-du-l%E1%BB%8Bch-ch%E1%BB%91ng-th%E1%BA%A5m-n%C6%B0%E1%BB%9Bc-i.126105032.23164579872?sp_atk=c5bbddef-6334-4461-a440-71d010b0e2a9&xptdk=c5bbddef-6334-4461-a440-71d010b0e2a9">
+            <div className="body-img">
+              <img src="/images/144.png" alt="" />
+              <div className="ml-[12px] text-[10px]">
+                <p className="font-bold">
+                  Hộp đựng sữa bột kèm thìa phi hành gia - Đại
+                </p>
+                <span className="text-red-500 font-medium mr-10">79.000</span>
+                <span>
+                  <del>99000</del>
+                </span>
+              </div>
+            </div>
+          </a>
+          <a href="https://shopee.vn/H%E1%BB%99p-%C4%91%E1%BB%B1ng-s%E1%BB%AFa-b%E1%BB%99t-k%C3%A8m-th%C3%ACa-g%E1%BA%A1t-ti%E1%BB%87n-d%E1%BB%A5ng-h%E1%BB%99p-%C4%91%E1%BB%B1ng-s%E1%BB%AFa-cho-b%C3%A9-khi-%C4%91i-ch%C6%A1i-%C4%91i-du-l%E1%BB%8Bch-ch%E1%BB%91ng-th%E1%BA%A5m-n%C6%B0%E1%BB%9Bc-i.126105032.23164579872?sp_atk=c5bbddef-6334-4461-a440-71d010b0e2a9&xptdk=c5bbddef-6334-4461-a440-71d010b0e2a9">
+            <div>
+              <img src="/images/147.png" alt="" />
+              <div className="ml-[12px] text-[10px]">
+                <p className="font-bold">
+                  Hộp đựng sữa bột kèm thìa phi hành gia - Trung
+                </p>
+                <span className="text-red-500 font-medium mr-10">74.000</span>
+                <span>
+                  <del>94.000</del>
+                </span>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="flex">
+          <a href="https://shopee.vn/C%E1%BB%8D-b%C3%ACnh-s%E1%BB%AFa-Silicon-g%E1%BA%A5p-g%E1%BB%8Dn-ti%E1%BB%87n-l%E1%BB%A3i-khi-mang-theo-d%E1%BB%85-d%C3%A0ng-1-h%E1%BB%99p-v%E1%BB%9Bi-6-ch%E1%BB%A9c-n%C4%83ng-d%C3%B9ng-cho-m%E1%BB%8Di-lo%E1%BA%A1i-b%C3%ACnh-i.126105032.24579507998?sp_atk=6a5ee632-74ef-4a98-ba29-8c84e52abdcb&xptdk=6a5ee632-74ef-4a98-ba29-8c84e52abdcb">
+            <div className="body-img">
+              <img src="/images/161.png" alt="" />
+              <div className="ml-[12px] text-[10px]">
+                <p className="font-bold">Cọ bình sữa Silicon gấp gọn</p>
+                <span className="text-red-500 font-medium mr-10">108.000</span>
+                <span>
+                  <del>120000</del>
+                </span>
+              </div>
+            </div>
+          </a>
+          <a href="https://shopee.vn/H%E1%BB%99p-chia-s%E1%BB%AFa-b%E1%BB%99t-JIAYI-h%C3%ACnh-g%E1%BA%A5u-cho-b%C3%A9-3-t%E1%BA%A7ng-c%C3%B3-quai-%C4%91eo-ti%E1%BB%87n-d%E1%BB%A5ng-nh%E1%BB%8F-g%E1%BB%8Dn-nh%C3%A0-bum-i.126105032.25680008266?sp_atk=aee70cc6-f0e3-4ed5-82fc-0296ebfb5c98&xptdk=aee70cc6-f0e3-4ed5-82fc-0296ebfb5c98">
+            <div>
+              <img src="/images/126.png" alt="" />
+              <div className="ml-[12px] text-[10px]">
+                <p className="font-bold">Hộp chia sữa bột JIAY</p>
+                <span className="text-red-500 font-medium mr-10">64000</span>
+                <span>
+                  <del>70000</del>
+                </span>
+              </div>
+            </div>
+          </a>
+        </div>
+        {/* <div className="flex">
+          <a href="https://shopee.vn/H%E1%BB%99p-%C4%91%E1%BB%B1ng-s%E1%BB%AFa-b%E1%BB%99t-ti%E1%BB%87n-d%E1%BB%A5ng-cho-b%C3%A9-khi-di-ch%C6%A1i-ho%E1%BA%B7c-khi-%C4%91i-ra-ngo%C3%A0i-i.126105032.25180593711?xptdk=5e01b134-ba73-47ac-806b-08ec12d044ed">
+            <div className="body-img">
+              <img src="/images/139.png" alt="" />
+              <div className="ml-[12px] text-[10px]">
+                <p className="font-bold">Yếm ăn silicon siêu mềm cao cấp</p>
+                <span className="text-red-500 font-medium mr-10">
+                  Đang cập nhật
+                </span>
+                <span>
+                  <del></del>
+                </span>
+              </div>
+            </div>
+          </a>
+          <a href="https://shopee.vn/H%E1%BB%99p-%C4%91%E1%BB%B1ng-s%E1%BB%AFa-b%E1%BB%99t-ti%E1%BB%87n-d%E1%BB%A5ng-cho-b%C3%A9-khi-di-ch%C6%A1i-ho%E1%BA%B7c-khi-%C4%91i-ra-ngo%C3%A0i-i.126105032.25180593711?xptdk=5e01b134-ba73-47ac-806b-08ec12d044ed">
+            <div>
+              <img src="/images/140.png" alt="" />
+              <div className="ml-[12px] text-[10px]">
+                <p className="font-bold">Yếm ăn silicon siêu mềm cao cấp</p>
+                <span className="text-red-500 font-medium mr-10">
+                  Đang cập nhật
+                </span>
+                <span>
+                  <del></del>
+                </span>
+              </div>
+            </div>
+          </a>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default BodyTemplate;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,13 @@
 import { RouteObject } from "react-router-dom";
 import { PATH } from "constant";
 import { AuthLayout } from "components";
+import BodyTemplate from "components/templates/BodyTemplate";
 export const router: RouteObject[] = [
+  {
+    path: "/",
+    element: <BodyTemplate />,
+  },
+
   {
     element: <AuthLayout />,
     children: [
